65682c30a8b520cd6012b4f16c8ae3608493b8a0
Valentin Toma <valentintoma1993@gmail.com>
2018-09-19 21:00:26 +0300

    Questions and Answers

---
+++ src/ro/jademy/milionaire/Game.java
@@ -0,0 +1,31 @@
+package ro.jademy.milionaire;
+
+import java.util.Random;
+
+public class Game {
+
+    private int curentLevel = 1;
+
+    public void start() {  //initilizam intrebarile si jocul
+
+
+
+    }
+
+    public void askQuestion() {
+        //show question on screen
+
+
+    }
+
+    public void answerQuestion() {
+        //get user imput and check if correct
+    }
+
+    public void fiftyFifty() {
+        //show the same question but with only two possible answers
+    }
+
+
+
+}
