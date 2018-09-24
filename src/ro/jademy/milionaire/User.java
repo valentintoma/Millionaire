3df52c8c7b8d2e8d1c72e582a6fba2364f5ac337
Valentin Toma <valentintoma1993@gmail.com>
2018-09-24 18:39:51 +0300

    Added Question Class

---
+++ src/ro/jademy/milionaire/User.java
@@ -0,0 +1,14 @@
+package ro.jademy.milionaire;
+
+public class User {
+
+    public String name;
+    public String surnName;
+    public int age;
+
+    public User(String name, String surnName, int age) {
+        this.name = name;
+        this.surnName = surnName;
+        this.age = age;
+    }
+}
